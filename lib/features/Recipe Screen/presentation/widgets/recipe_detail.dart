d77cef4e752e51d4b1090441bc546196ff5dd25e
Shristi Rai <shristirai.959@gmail.com>
2024-11-14 23:47:35 +0545

    adding skeleton loading

--- lib/features/Recipe Screen/presentation/widgets/recipe_detail.dart
+++ lib/features/Recipe Screen/presentation/widgets/recipe_detail.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:newproject/features/Recipe%20Screen/domain/recipe.dart';
 import 'package:newproject/features/Recipe%20Screen/presentation/state/state.dart';
 import 'package:newproject/utils/exporter.dart';
+import 'package:shimmer/shimmer.dart';
 
 class RecipeDetail extends ConsumerWidget {
-  const RecipeDetail({super.key, required this.recipe});
+  const RecipeDetail({super.key, required this.recipe, this.loading = true});
   final Recipe recipe;
+  final bool loading;
   @override
   Widget build(BuildContext context, ref) {
     final recipeProvider = recipeDetailProvider(recipe.id!);
@@ -20,185 +22,7 @@ class RecipeDetail extends ConsumerWidget {
                   // return Future.value();
                   return singleRecipeController.refresh(recipe.id!);
                 },
-                child: ListView(
-                  children: [
-                    Stack(
-                      children: [
-                        Container(
-                          alignment: Alignment.topLeft,
-                          width: double.infinity,
-                          height: 350,
-                          decoration: BoxDecoration(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(30)),
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: NetworkImage(details.image!))),
-                          child: GestureDetector(
-                            onTap: () {
-                              Navigator.pop(context);
-                            },
-                            child: Container(
-                              width: 40,
-                              height: 40,
-                              margin: const EdgeInsets.only(left: 10, top: 20),
-                              padding: const EdgeInsets.only(left: 8),
-                              decoration: const BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10)),
-                                  color: Color.fromARGB(255, 59, 66, 83)),
-                              child: const Icon(
-                                Icons.arrow_back_ios,
-                                color: Colors.white70,
-                                size: 18,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          margin: const EdgeInsets.only(
-                              top: 300, left: 15, right: 15),
-                          decoration: const BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  blurRadius: 1,
-                                  offset: Offset(0, 1),
-                                  color: Color.fromARGB(255, 130, 127, 127),
-                                )
-                              ],
-                              color: Colors.white,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(20))),
-                          child: Column(
-                            children: [
-                              Text(
-                                "${details.title}",
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 25,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                              Text(
-                                "${details.extendedIngredients?.length} ingredients",
-                                style: const TextStyle(
-                                    color: Color.fromARGB(202, 104, 102, 102)),
-                              ),
-                              Container(
-                                margin: const EdgeInsets.only(top: 15),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    recipeSpecs(
-                                        icon: Icons.timer,
-                                        specification:
-                                            "${details.cookingMinutes ?? " "} min"),
-                                    recipeSpecs(
-                                        specification:
-                                            "${details.healthScore} H.Score",
-                                        icon: Icons
-                                            .local_fire_department_outlined),
-                                    recipeSpecs(
-                                        specification:
-                                            "${details.servings} servings",
-                                        icon: Icons.adjust)
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 15),
-                      child: const Text(
-                        "Ingredients",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 130,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        padding: const EdgeInsets.only(left: 20),
-                        children: [
-                          Row(
-                            children: [
-                              if (details.extendedIngredients!.length > 0)
-                                for (int i = 0;
-                                    i < details.extendedIngredients!.length;
-                                    i++)
-                                  eachIngredients(
-                                      image: details
-                                              .extendedIngredients![i].image ??
-                                          "",
-                                      name:
-                                          details.extendedIngredients![i].name!,
-                                      quantity: details
-                                          .extendedIngredients![i].amount!),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 15),
-                      child: const Text(
-                        "Cooking instruction",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
-                      ),
-                    ),
-                    Container(
-                      width: double.infinity,
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 10),
-                      decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                          color: Color.fromRGBO(255, 246, 234, 1)),
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          if (details.analyzedInstructions != null &&
-                              details.analyzedInstructions!.isNotEmpty)
-                            for (var instruction
-                                in details.analyzedInstructions!)
-                              for (var step in instruction["steps"]!)
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      "Step ${step["number"]}",
-                                      style: const TextStyle(
-                                          color: Colors.orange,
-                                          fontWeight: FontWeight.w600,
-                                          letterSpacing: 1,
-                                          fontSize: 15),
-                                    ),
-                                    Container(
-                                      margin: const EdgeInsets.symmetric(
-                                          vertical: 8),
-                                      child: Text(
-                                        "${step["step"]}",
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.w500,
-                                            letterSpacing: 1,
-                                            fontSize: 14),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                child: listViewUI(details, context, loading: false),
               );
             },
             error: (error, stackTrace) {
@@ -207,7 +31,176 @@ class RecipeDetail extends ConsumerWidget {
                 child: Text(error.toString()),
               );
             },
-            loading: () => const Center(child: CircularProgressIndicator())));
+            loading: () => AbsorbPointer(
+                child: listViewUI(Recipe(), context, loading: false))));
+  }
+
+  ListView listViewUI(Recipe details, BuildContext context,
+      {bool loading = true}) {
+    return ListView(
+      children: [
+        Stack(
+          children: [
+            loading
+                ? ShimmerBox()
+                : Container(
+                    alignment: Alignment.topLeft,
+                    width: double.infinity,
+                    height: 350,
+                    decoration: BoxDecoration(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(30)),
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(details.image ?? ""))),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        width: 40,
+                        height: 40,
+                        margin: const EdgeInsets.only(left: 10, top: 20),
+                        padding: const EdgeInsets.only(left: 8),
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Color.fromARGB(255, 59, 66, 83)),
+                        child: const Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.white70,
+                          size: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.only(top: 280, left: 15, right: 15),
+              decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 1,
+                      offset: Offset(0, 1),
+                      color: Color.fromARGB(255, 130, 127, 127),
+                    )
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Column(
+                children: [
+                  Text(
+                    "${details.title}",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    "${details.extendedIngredients?.length} ingredients",
+                    style: const TextStyle(
+                        color: Color.fromARGB(202, 104, 102, 102)),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        recipeSpecs(
+                            icon: Icons.timer,
+                            specification:
+                                "${details.cookingMinutes ?? " "} min"),
+                        recipeSpecs(
+                            specification: "${details.healthScore} H.Score",
+                            icon: Icons.local_fire_department_outlined),
+                        recipeSpecs(
+                            specification: "${details.servings} servings",
+                            icon: Icons.adjust)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: const Text(
+            "Ingredients",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        ),
+        SizedBox(
+          height: 130,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            children: [
+              Row(
+                children: [
+                  if (details.extendedIngredients!.length > 0)
+                    for (int i = 0;
+                        i < details.extendedIngredients!.length;
+                        i++)
+                      eachIngredients(
+                          image: details.extendedIngredients![i].image ?? "",
+                          name: details.extendedIngredients![i].name!,
+                          quantity: details.extendedIngredients![i].amount!),
+                ],
+              )
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 15),
+          child: const Text(
+            "Cooking instruction",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              color: Color.fromRGBO(255, 246, 234, 1)),
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (details.analyzedInstructions != null &&
+                  details.analyzedInstructions!.isNotEmpty)
+                for (var instruction in details.analyzedInstructions!)
+                  for (var step in instruction["steps"]!)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Step ${step["number"]}",
+                          style: const TextStyle(
+                              color: Colors.orange,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 1,
+                              fontSize: 15),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(vertical: 8),
+                          child: Text(
+                            "${step["step"]}",
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: 1,
+                                fontSize: 14),
+                          ),
+                        )
+                      ],
+                    ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 
   Column eachIngredients(
@@ -243,7 +236,7 @@ class RecipeDetail extends ConsumerWidget {
             child: Text(
               name,
               style: const TextStyle(
-                fontWeight: FontWeight.w600,
+                fontWeight: FontWeight.w500,
               ),
             ),
           ),
@@ -255,6 +248,22 @@ class RecipeDetail extends ConsumerWidget {
           ),
         )
       ],
+    );
+  }
+
+  Shimmer ShimmerBox(
+      {double? height, double? width, BoxShape shape = BoxShape.rectangle}) {
+    return Shimmer.fromColors(
+      baseColor: Color.fromARGB(255, 97, 97, 97),
+      highlightColor: Colors.grey,
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          shape: shape,
+          color: Colors.grey,
+        ),
+      ),
     );
   }
 
